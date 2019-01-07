adc4ab8a1fc8363a08aedc3b809e4f7b2aeecde4
BangHeru <bang.heru82@gmail.com>
2019-01-07 16:04:23 +0700

    modif

--- pelanggan/views.py
+++ pelanggan/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from django.http import HttpResponse
-import json
 from django.core import serializers
+from django.db.models import Q
 
 from .models import Pelanggan
-# Create your views here.
+
 
 
 def home(request):
@@ -14,22 +13,18 @@ def home(request):
 
 
 def cari_pelanggan(request):
-    # if request.method == 'POST':
     if request.POST and request.is_ajax:
         kata_kunci = request.POST['search']
     else:
         kata_kunci = ''
  
+    lookups = Q(nama__startswith=kata_kunci) | Q(alamat__startswith=kata_kunci) | Q(telepon__startswith=kata_kunci)
 
-    # hasil_cari = Pelanggan.objects.filter(nama__contains=kata_kunci, 
-    #             alamat__contains=kata_kunci, telepon__contain=kata_kunci)
+    data = Pelanggan.objects.filter(lookups)
 
-    data = Pelanggan.objects.filter(nama__contains=kata_kunci)
-    
-    # return HttpResponse(kata_kunci)
+    # data = Pelanggan.objects.filter(nama__contains=kata_kunci)
+   
     data = serializers.serialize('json', list(data))
-    # print(data)
-    return HttpResponse(data, content_type="application/json")
     
-    # print(hasil_cari)
-    # return JsonResponse({'data':data})
+    return HttpResponse(data, content_type="application/json")
+  
